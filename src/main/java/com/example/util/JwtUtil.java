8d9f648cb20977fb5651edde46b3e6b3aa7759d1
tkddn2356 <tkddn2356@gmail.com>
2020-12-10 10:19:32 +0900

    test

--- src/main/java/com/example/util/JwtUtil.java
+++ src/main/java/com/example/util/JwtUtil.java
@@ -18,6 +18,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.xml.bind.DatatypeConverter;
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class JwtUtil {
@@ -38,13 +40,29 @@ public class JwtUtil {
     }
 
     public String generate(String id) {
-        Date exp = DateUtil.addHoursToJavaUtilDate(new Date(), 24);
-        return Jwts.builder().setSubject(String.valueOf(id)).setExpiration(exp).signWith(key, SignatureAlgorithm.HS256).compact();
+        // header
+        Map<String, Object> headers = new HashMap<>();
+        headers.put("typ", "JWT");
+        headers.put("alg", "HS256");
+
+        // payload
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("sub", id);
+        claims.put("exp", DateUtil.addHoursToJavaUtilDate(new Date(), 24));
+
+        // Generate token
+        String token = Jwts.builder()
+                .setHeader(headers)
+                .setClaims(claims)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
+        return token;
     }
 
     public boolean isExpired(String token){
-        Claims body = Jwts.parser().setSigningKey(this.key).parseClaimsJws(token).getBody();
-        Date exp = body.get("exp", Date.class);
+        Claims claims  = Jwts.parser().setSigningKey(this.key).parseClaimsJws(token).getBody();
+        Date exp = claims .get("exp", Date.class);
         Date now = new Date();
         if( exp.getTime() < now.getTime()){
             return true;
@@ -66,8 +84,8 @@ public class JwtUtil {
         String authToken = header.substring(7);
 
         if(isExpired(authToken)==false){
-            Claims body = Jwts.parser().setSigningKey(this.key).parseClaimsJws(authToken).getBody();
-            String account_id = body.getSubject();
+            Claims claims  = Jwts.parser().setSigningKey(this.key).parseClaimsJws(authToken).getBody();
+            String account_id = claims.getSubject();
             User user = userMapper.getUserByAccountId(account_id);
             return user;
         }
