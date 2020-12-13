fec1409f8f7b8384114b16d455206554320f15fc
tngus <tngus3722@koreatech.ac.kr>
2020-12-13 23:45:36 +0900

    1213.

--- src/main/resources/sql/create.sql
+++ src/main/resources/sql/create.sql
@@ -1,24 +1,22 @@
 CREATE DATABASE IF NOT EXISTS beginner_example_database;
 DROP TABLE IF EXISTS  beginner_example_database.board;
-create table board(
-	id bigint auto_increment,
+create table beginner_example_database.board(
+	id bigint unsigned auto_increment primary key,
     title varchar(200) not null,
     content varchar(3000) not null,
     user_id varchar(50) not null,
     nickname varchar(50),
-    created_at timestamp default CURRENT_TIMESTAMP(),
-    updated_at timestamp default CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
-    constraint board_PK primary key(id)
+	updated_at timestamp default current_timestamp,
+    created_at timestamp default current_timestamp
 );
-DROP TABLE IF EXISTS  beginner_example_database.user;
-create table user(
-    id bigint auto_increment,
-    account_id varchar(100) not null,
+
+create table beginner_example_database.user(
+    id bigint unsigned auto_increment primary key,
+    account_id varchar(100) unique not null,
     password varchar(100) not null,
     name varchar(50) not null,
     nickname varchar(50) not null,
-    created_at timestamp default CURRENT_TIMESTAMP(),
-    updated_at timestamp default CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
-    constraint user_PK primary key (id)
+    updated_at timestamp default current_timestamp,
+    created_at timestamp default current_timestamp
 );
 <!-- FIX 정수현 :: token sql 삭제-->
